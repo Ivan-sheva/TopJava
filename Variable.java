7fe1a1921ba8b0ff954d89db00a3d462f4f0a86b
Ivan Nikolaenko <ivan-nikolaenko@bk.ru>
2020-09-07 22:13:24 +0600

    Lesson1

---
+++ Variable.java
@@ -0,0 +1,18 @@
+ public class Variable {
+     public static void main(String[] args) {
+          byte numberOfProcessorCores=4;
+          short systemTypeWindows=64;
+          int laptopPrice$=900;
+          long hddSizeGb= 1000L;
+          float weightLaptopKg=3.7f;
+          double DDR=8.0;
+          char chtoTo='\u2764';
+          boolean bestLaptop = true;
+          System.out.println(numberOfProcessorCores+"\n"+systemTypeWindows+"\n"+laptopPrice$+"\n"+
+                  hddSizeGb+"\n"+weightLaptopKg+"\n"+DDR+"\n"+chtoTo+"\n"+bestLaptop);
+
+
+
+
+     }
+}
