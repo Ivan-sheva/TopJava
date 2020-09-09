2ff7903f38bd81fec1429b5203cf77ea9c4578e4
Ivan Nikolaenko <ivan-nikolaenko@bk.ru>
2020-09-09 19:28:42 +0600

    Lesson2

--- ConditionalStatement.java
+++ ConditionalStatement.java
@@ -1,23 +1,26 @@
 public class ConditionalStatement {
     public static void main(String[] args) {
         int age = 25;
-        char sex = 'M';
-        double growth = 1.60;
-        char firstLetterOfName = 'I';
         if (age < 20) {
             System.out.println("Ваш возраст меньше 20");
         }
+
+        char sex = 'M';
         if (sex == 'M') {
             System.out.println("Вы мужского пола");
         }
         if (sex != 'M') {
             System.out.println("Вы женского рода");
         }
+
+        double growth = 1.60;
         if (growth < 1.80) {
             System.out.println("Ваш рост меньше 1.80");
         } else {
             System.out.println("Ваш рост больше 1.80");
         }
+
+        char firstLetterOfName = 'I';
         if (firstLetterOfName == 'M') {
             System.out.println("Первая буква вашего имени М");
         } else if (firstLetterOfName == 'I') {
