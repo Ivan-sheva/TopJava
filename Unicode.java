e38003ae2270c104f2143fb57beba08ce44c67c0
Ivan Nikolaenko <ivan-nikolaenko@bk.ru>
2020-09-11 17:05:48 +0600

    Unicode

---
+++ Unicode.java
@@ -0,0 +1,7 @@
+public class Unicode {
+    public static void main(String[] args) {
+        for (int i = 0x9398; i <= 0x10178; i++) {
+            System.out.println((char) i);
+        }
+    }
+}
